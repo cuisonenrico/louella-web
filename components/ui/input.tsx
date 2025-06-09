d24d394b9b32cb28e7f19bcc393bfa7079d0f284
Enrico Cuison <cuisonenrico@gmail.com>
2025-06-09 15:17:20 +0800

    Update

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         type={type}
         className={cn(
           "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base text-foreground shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          "autofill:text-foreground autofill:shadow-[inset_0_0_0px_1000px_rgb(255,255,255)]",
+          "autofill:text-foreground",
           className
         )}
         ref={ref}
