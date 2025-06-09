bafcccec0e09ff2aaf09ff5ea4a1265536db13da
Enrico Cuison <cuisonenrico@gmail.com>
2025-06-09 14:38:01 +0800

    Color and UI UX improvements

--- app/dashboard/payroll/page.tsx
+++ app/dashboard/payroll/page.tsx
@@ -1,35 +1,7 @@
-"use client";
 
 import PayrollTable from "@/components/custom/payroll_table";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import * as XLSX from 'xlsx';
 
 export default function Page() {
-    const [data, setData] = useState<any[]>([]);
-
-    // Function to handle the file upload
-    const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0];
-        if (!file) return;
-
-        const reader = new FileReader();
-        reader.onload = (evt) => {
-            const bstr = evt.target?.result;
-            const wb = XLSX.read(bstr, { type: 'binary' });
-
-            // Read first sheet
-            const wsName = wb.SheetNames[0];
-            const ws = wb.Sheets[wsName];
-
-            // Convert sheet to JSON
-            const jsonData = XLSX.utils.sheet_to_json(ws, { header: 1 });
-            const trimmedData = (jsonData as any[][]).slice(4).slice(4, (jsonData as any[][]).length - 13);
-            setData(trimmedData);
-        };
-
-        reader.readAsArrayBuffer(file);
-    };
 
     return (
         <div className="w-full h-full flex flex-col">
